d2d85d440c9596bd677df867831ecb6d4a8cb60a
lamartire <la_martire@mail.ru>
2017-03-03 22:13:20 +0300

    Include client functions to core file

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-module.exports = require('./lib/redbone');
+var redboneCore = require('./lib/redbone'),
+    getServerDispatchMidddleware = require('./client/getServerDispatchMidddleware');
+
+/**
+ * Incuding client helper function to core file
+ * for more confortable importing
+ */
+
+redboneCore.getServerDispatchMidddleware = getServerDispatchMidddleware;
+
+module.exports = redboneCore;
