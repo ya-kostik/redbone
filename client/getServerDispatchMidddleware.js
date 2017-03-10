550dfce2e6fc59e7b4acaf3a79308472d933d61d
lamartire <la_martire@mail.ru>
2017-03-10 19:47:31 +0300

    exclude parameter to middleware

--- client/getServerDispatchMidddleware.js
+++ client/getServerDispatchMidddleware.js
@@ -3,14 +3,17 @@ const merge = require('lodash/merge');
 
 var server_regexp = /^@@server\/.*?$/i
 var defaultOptions = {
-  next: false
+  next: false,
+  exclude: null
 }
 
 /**
  * Process client -> server dispatching
- * @param  {Socket} io socket.io connection to server
- * @param  {Object} options action processing options
- * @return {Mided}  do not use it
+ * @param  {Socket} io - socket.io connection to server
+ * @param  {Object} options - action processing options
+ * @param  {Boolean} options.next - process server action or not
+ * @param  {Array} options.exclude - list of disallowed server action types
+ * @return {Mided}  - do not use it
  */
 module.exports = function getServerDispatchMiddleware(io, options) {
   let middlewareOptions = options ? merge(defaultOptions, options) : defaultOptions;
@@ -21,10 +24,12 @@ module.exports = function getServerDispatchMiddleware(io, options) {
         if (!isPlainObject(action)) return next(action);
         if (!action.type) return next(action);
         if (server_regexp.test(action.type)) {
-          if (middlewareOptions.next === false) {
-            return io.emit('dispatch', action);
-          } else {
-            io.emit('dispatch', action);
+          if (!middlewareOptions.exclude || middlewareOptions.exclude.indexOf(action.type) === -1) {
+            if (middlewareOptions.next === false) {
+              return io.emit('dispatch', action);
+            } else {
+              io.emit('dispatch', action);
+            }
           }
         }
         next(action);
