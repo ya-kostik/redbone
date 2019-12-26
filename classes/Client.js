432d9b89c3ab703bc226086d0f4650f3c319f976
 Constantin Tsukanov <c@indotech.ru>
2019-12-26 16:02:02 +0300

    Add JSDoc for the Redbone class

--- classes/Client.js
+++ classes/Client.js
@@ -1,6 +1,4 @@
-const EventEmitter = require('events');
-
-class Client extends EventEmitter {
+class Client {
 
 }
 
--- classes/Redbone.js
+++ classes/Redbone.js
@@ -1,6 +1,20 @@
 const isNot = require('../lib/isNot');
 const Middleware = require('./Middleware');
 
+/**
+ * Handler type for a middleware or watcher
+ * @typedef {(String|RegExp|null)} HandlerType
+ */
+
+/**
+ * @typedef {Object} Action
+ * @prop    {String} type
+ */
+
+/**
+ * @class Redbone
+ * Simple two way dispatcher
+ */
 class Redbone {
   constructor(transport = null, options = {}) {
     this.transport = transport;
@@ -11,12 +25,23 @@ class Redbone {
     this.after = new Middleware(this);
   }
 
+  /**
+   * Watch for a type
+   * @param  {HandlerType} type — the type of action to watch
+   * @param  {Function}    watcher — is a watcher's handler
+   * @return {Redbone}     this
+   */
   watch(type, watcher) {
     this.watchers.use(type, watcher);
     // It is need for chaining
     return this;
   }
 
+  /**
+   * Set catcher for errors
+   * @param  {Function} catcher
+   * @return {Redbone}  this
+   */
   catch(catcher) {
     isNot(catcher, 'function', 'catcher');
     this.catcher = catcher;
@@ -24,16 +49,35 @@ class Redbone {
     return this;
   }
 
+  /**
+   * Use `before` middleware
+   * @param  {HandlerType} type — the type of action
+   * @param  {Function}    middleware — is a middleware's handler
+   * @return {Redbone}     this
+   */
   use(type, middleware) {
     this.before.use(type, middleware);
     // It is need for chaining
     return this;
   }
 
+  /**
+   * Bind redbone to client
+   * @param  {Client}  client — is an instance of Client
+   * @return {Redbone} this
+   */
   bind(client) {
     if (!client.redbone) client.redbone = this;
+
+    return this;
   }
 
+  /**
+   * Dispatch action to redbone's process chain
+   * @param  {Client}  client — is an instance of Client
+   * @param  {Action}  action — is an action to process
+   * @return {[type]}        [description]
+   */
   async dispatch(client, action) {
     try {
       isNot(action, 'valid action', 'action');
@@ -46,6 +90,11 @@ class Redbone {
     }
   }
 
+  /**
+   * Default catcher for errors
+   * Just throws errors next
+   * @param  {Error} err
+   */
   catcher(err) {
     throw err;
   }
